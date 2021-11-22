26c1c6cbc239b2b2de6b926887cae6bce5ebe7fe
Mark Ulrich <31822@ma-web.nl>
2021-11-22 19:01:15 +0100

    Added summaries to all currently existing classes and functions.

--- Database/Faction.cs
+++ Database/Faction.cs
@@ -4,9 +4,25 @@ using System.Text;
 
 namespace Database
 {
+    /// <summary>
+    /// Base class for all Factions.
+    ///     <para>
+    ///         <description>Member Variables:</description>
+    ///     </para>
+    ///     <list type="bullet">
+    ///         <item>
+    ///             <term>_name</term>
+    ///             <description>Name of the Faction</description>
+    ///         </item>
+    ///     </list>
+    /// </summary>
     class Faction
     {
         public string _name;
+        
+        /// <summary>
+        /// Constructor to set base values for a Faction.
+        /// </summary>
         public Faction(string aName)
         {
             _name = aName;
