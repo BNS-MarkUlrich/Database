b00fc9298b726f4a2f730e8a49af92c493a66d61
BNS-MarkUlrich <31822@ma-web.nl>
2021-11-29 15:43:48 +0100

    Assignment #2

--- Database/Database.cs
+++ Database/Database.cs
@@ -4,20 +4,39 @@ using System.Text;
 
 namespace Database
 {
+    /// <summary>
+    /// Keeps the Faction information and allows adding, editing and removing of Factions.
+    ///     <para>
+    ///         <description>Member Variables:</description>
+    ///     </para>
+    ///     <list type="bullet">
+    ///         <item>
+    ///             <term>_factions</term>
+    ///             <description>Contains a list of factions</description>
+    ///         </item>
+    ///     </list>
+    /// </summary>
     public class Database
     {
         private List<Faction> _factions;
 
+        /// <summary>
+        /// Instantiates a new list of Factions.
+        /// </summary>
         public Database()
         {
             _factions = new List<Faction>();
         }
 
+        /// <summary>
+        /// Allows you to add new Factions to the list.
+        /// </summary>
         public void AddItem(Faction faction)
         {
             _factions.Add(faction);
         }
 
+        /// <returns>All items in the list.</returns>
         public Faction[] GetAllItems()
         {
             return _factions.ToArray();
--- Database/Faction.cs
+++ Database/Faction.cs
@@ -4,12 +4,26 @@ using System.Text;
 
 namespace Database
 {
+    /// <summary>
+    /// Base class for all Factions.
+    ///     <para>
+    ///         <description>Member Variables:</description>
+    ///     </para>
+    ///     <list type="bullet">
+    ///         <item>
+    ///             <term>_name</term>
+    ///             <description>Name of the Faction</description>
+    ///         </item>
+    ///     </list>
+    /// </summary>
     public abstract class Faction
     {
         public string _appearance { get; private set; }
         public string name { get; private set; }
-       
 
+        /// <summary>
+        /// Constructor to set base values for a Faction.
+        /// </summary>
         public Faction(string anAppearance, string aName)
         {
             this._appearance = anAppearance;
